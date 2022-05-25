3c83eb4882e9b6397f9717c31cc7807577a7aa6d
Shoxruh <=>
2022-05-25 23:18:56 +0500

    contact section finish

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./_contact.scss";
 import "../main.scss";
-import signUp from "../../Assets/images/signUp.png";
+
 import phone from "../../Assets/images/phone.png";
 import message from "../../Assets/images/message.png";
 import location from "../../Assets/images/location.png";
@@ -53,11 +53,13 @@ function Subscribe() {
           <div className="map">
             <div className="google-map">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.525686448247!2d69.28454511526243!3d41.340926279268416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b534175ed31%3A0x52a8f9d9414a2ad8!2z0KLQsNGI0LrQtdC90YLRgdC60LjQuSDQo9C90LjQstC10YDRgdC40YLQtdGCINCY0L3RhNC-0YDQvNCw0YbQuNC-0L3QvdGL0YUg0KLQtdGF0L3QvtC70L7Qs9C40Lk!5e0!3m2!1sru!2s!4v1653484788281!5m2!1sru!2s"
-                width="570"
-                height="360"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.525686448254!2d69.28454511526243!3d41.340926279268416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b534175ed31%3A0x52a8f9d9414a2ad8!2z0KLQsNGI0LrQtdC90YLRgdC60LjQuSDQo9C90LjQstC10YDRgdC40YLQtdGCINCY0L3RhNC-0YDQvNCw0YbQuNC-0L3QvdGL0YUg0KLQtdGF0L3QvtC70L7Qs9C40Lk!5e0!3m2!1sru!2s!4v1653497562990!5m2!1sru!2s"
+                // width="600"
+                height="450"
                 // style="border:0;"
+                // allowfullscreen=""
                 loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
               ></iframe>
             </div>
             <div className="connection">
